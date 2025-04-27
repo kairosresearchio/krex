d5de25e379fed81e08fc01bdc7ef2eb76edccb62
HONG,HAO-SYUAN <pg785632541@gmail.com>
2025-04-27 11:31:06 +0800

    update okx

--- krex/okx/endpoints/trade.py
+++ krex/okx/endpoints/trade.py
@@ -3,7 +3,6 @@ from enum import Enum
 
 class Trade(str, Enum):
     PLACE_ORDER = "/api/v5/trade/order"
-    # BATCH_ORDERS = "/api/v5/trade/batch-orders"
     CANCEL_ORDER = "/api/v5/trade/cancel-order"
     CANCEL_BATCH_ORDERS = "/api/v5/trade/cancel-batch-orders"
     AMEND_ORDER = "/api/v5/trade/amend-order"
@@ -15,9 +14,4 @@ class Trade(str, Enum):
     ORDERS_HISTORY_ARCHIVE = "/api/v5/trade/orders-history-archive"
     ORDER_FILLS = "/api/v5/trade/fills"
     ORDERS_FILLS_HISTORY = "/api/v5/trade/fills-history"
-    ONE_CLICK_REPAY_SUPPORT = "/api/v5/trade/one-click-repay-currency-list"
-    ONE_CLICK_REPAY = "/api/v5/trade/one-click-repay"
-    ONE_CLICK_REPAY_HISTORY = "/api/v5/trade/one-click-repay-history"
-    # MASS_CANCEL = "/api/v5/trade/mass-cancel"
-    # CANCEL_ALL_AFTER = "/api/v5/trade/cancel-all-after"
     ACCOUNT_RATE_LIMIT = "/api/v5/trade/account-rate-limit"
