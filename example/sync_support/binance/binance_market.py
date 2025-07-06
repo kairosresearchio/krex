1a389a368bd3fa4884db3ea37ac05259a6e7bc43
HONG,HAO-SYUAN <pg785632541@gmail.com>
2025-07-06 14:30:55 +0800

    fix(binance): add missing code

--- example/sync_support/binance/binance_market.py
+++ example/sync_support/binance/binance_market.py
@@ -12,7 +12,7 @@ print(result)
 result = client.get_futures_ticker(product_symbol="BTC-USDT-SWAP")
 print(result)
 
-result = client.get_futures_kline(product_symbol="BTC-USDT-SWAP", interval="1m")
+result = client.get_kline(product_symbol="BTC-USDT-SWAP", interval="1m")
 print(result)
 
 result = client.get_futures_premium_index(product_symbol="BTC-USDT-SWAP")
--- krex/binance/endpoints/account.py
+++ krex/binance/endpoints/account.py
@@ -1,6 +1,13 @@
 from enum import Enum
 
 
+class SpotAccount(str, Enum):
+    ACCOUNT_BALANCE = "/api/v3/account"
+
+    def __str__(self) -> str:
+        return self.value
+
+
 class FuturesAccount(str, Enum):
     ACCOUNT_BALANCE = "/fapi/v3/balance"
     INCOME_HISTORY = "/fapi/v1/income"
