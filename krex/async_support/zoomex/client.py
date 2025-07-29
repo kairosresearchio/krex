038b9bb5a7658798e60d2eba0fdb33f30ba2163a
JoeChang <joechang@JoeChangs-MacBook-Pro.local>
2025-07-29 10:18:34 +0800

    feat(Async-Zoomex): Zoomex add get_wallet_ballance

--- krex/async_support/zoomex/client.py
+++ krex/async_support/zoomex/client.py
@@ -1,10 +1,12 @@
 from ._trade_http import TradeHTTP
 from ._market_http import MarketHTTP
+from ._account_http import AccountHTTP
 
 
 class Client(
     TradeHTTP,
     MarketHTTP,
+    AccountHTTP,
 ):
     def __init__(self, **args):
         super().__init__(**args)
